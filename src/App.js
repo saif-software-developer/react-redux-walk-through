0a9d313646535cc10ff5218eed2c68ea6aa11cbb
Saif Masadeh <saifmasadeh@gmail.com>
2018-09-11 15:38:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{ Component } from 'react';
 import './App.css';
 import Counter from './Counter.js';
+// 5: import the store to pass it to the components 
 import store from './store';
 
 class App extends Component {
