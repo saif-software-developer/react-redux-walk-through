24e9e706032bd9275334254613772d519272f604
Saif Masadeh <saifmasadeh@gmail.com>
2018-09-11 15:37:08 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
+// 1 : import createStore from redux 
 import {createStore} from 'redux';
 
+// 2 : Initialize the store state  
 const initState = {
 	count : 5
 };
 
+// 3 : Create the reducer function to handle actions 
 const  reducer = (state= initState, action)=> {
 	console.log('action type', action.type);
 
@@ -15,6 +18,7 @@ const  reducer = (state= initState, action)=> {
 	}
 }
 
+// 4 : Create the store and export it 
 const store = createStore(reducer,initState,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store; 
